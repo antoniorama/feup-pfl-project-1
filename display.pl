7a99bfb61657c3aa05e498a9b44f55468826d686
Miguel Pedrosa <93871576+migueljcpedrosa@users.noreply.github.com>
2023-10-18 16:54:03 +0100

    Defined draw board functions

--- display.pl
+++ display.pl
@@ -1,62 +1,39 @@
-% display_coordinates_bottom(+Length)
-% Displays the pattern '1  2  3  4  ... Length'
-% For our board should be dispalyed with length 9
-%display_coordinates_bottom(0) :-
-%	write(0).
+% Display a bar of '+' and '-'
+display_bar(0) :- write('+\n').
+display_bar(Size) :-
+    write('+---'),
+    NewSize is Size - 1,
+    display_bar(NewSize).
 
-%display_coordinates_bottom(N):-
-%	N > 0,
-%	N1 is N - 1,
-%	display_coordinates_bottom(N1),
-%	write(N),
-%	write('  ').
+% Display a row of board cells
+display_row(0, RowNum) :- format("~d\n", [RowNum]).
+display_row(Size, RowNum) :-
+    write('   |'),
+    NewSize is Size - 1,
+    display_row(NewSize, RowNum).
 
-% Size of the board
-size(10).
-
-% Displays the horizontal bar for the board
-display_bar(0):- 
-    write('|\n'), !.
-display_bar(N):- 
-    write('|-----'),
-    N1 is N - 1,
-    display_bar(N1).
-
-% Displays the header of the board
-display_header(0, _):- 
-    nl, !.
-display_header(N, Size):- 
-    (N < 10 -> write('    ') ; write('   ')), % Proper spacing
-    write(N),
-    N1 is N - 1,
-    display_header(N1, Size).
-
-% Display one cell of the board
-display_cell:- 
-    write('         |').
-
-% Display one row of the board
-display_row(_, 0, _):- 
-    write('\n'), !.
-display_row(Line, Col, Size):- 
-    display_cell,
-    NextCol is Col - 1,
-    display_row(Line, NextCol, Size).
-
-% Display the entire board
-display_board(0, _):- 
-    !.
-display_board(Line, Size):- 
-    write(Line), write(' |'),
-    display_row(Line, Size, Size),
+% Display the board
+display_board(-1, _).
+display_board(RowNum, Size) :-
     display_bar(Size),
-    NextLine is Line - 10,
-    display_board(NextLine, Size).
+    display_row(Size, RowNum),
+    NewRowNum is RowNum - 1,
+    display_board(NewRowNum, Size).
 
-% Main predicate to display the empty board
-show_board:- 
-    size(Size),
-    display_header(Size, Size),
-    write('       '),
-    display_bar(Size),
-    display_board(90, Size).
+% Display the header
+display_header(-1) :- nl.
+display_header(Num) :-
+    format(' ~d ', [Num]),
+    NewNum is Num - 1,
+    display_header(NewNum).
+
+% To reverse the board coordinates as displayed in the image
+reverse_board_coordinates(Size, NewSize) :-
+    NewSize is Size - 1.
+
+% Test with a board of size 10
+test_display :-
+    Size = 10,
+    reverse_board_coordinates(Size, NewSize),
+    display_header(NewSize),
+    display_board(NewSize, Size).
